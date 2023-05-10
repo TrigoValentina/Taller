1aecba76e5ed00751c5cc4bf8ebf31c8a28a8b8d
TrigoValentina <tme0032929@est.univalle.edu>
2023-05-10 14:18:07 -0400

    mi primer hola mundo

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            int num1,num2,suma;
+            Console.WriteLine("ingrese un numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese un numero");
+            num2 = int.Parse(Console.ReadLine());
+            suma = num1 + num2;
+            Console.WriteLine("la suma es "+suma);
+
+
+        }
+    }
+}
